27bca1bb81db5acdcf9e91190234d4702ccc2fa1
Sawyer Vaughan <sawyerv726@gmail.com>
2017-01-23 11:28:20 -0800

    Refactored and compiled with -Wall -pedantic

--- hw01/cards.c
+++ hw01/cards.c
@@ -1,39 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define END_CHAR 'X'
+#define END_FLAG -2
+
+/*
+This function prompts the user for a card name,
+and will store it in the given card_name buffer.
+*/
+void get_card_name(char card_name[]) {
+  puts("Enter the card name: ");
+  scanf("%2s", card_name);
+}
+
+/*
+This function gets the value of the provided card
+name. For face cards, this will return 10. For an
+ace ('A'), this function will return 11. If the
+END_CHAR is provided, this function will return 
+the END_FLAG. The function will return the value
+as an integer if it lies within 1-10, otherwise
+will return -1
+*/
+int get_val(char card_name[]) {
+  switch(card_name[0]) {
+    case 'K':
+    case 'Q':
+    case 'J':
+      return 10;
+    case 'A':
+      return 11;
+    case END_CHAR:
+      return END_FLAG;
+    default:
+    {
+      int val = atoi(card_name);
+      if (val < 1 || val > 10) {
+        return -1;
+      }
+      return val;
+    }
+  }
+}
+
+/*
+This function returns the provided count variable
+incremented by either +1 or -1. If the value is 
+between 3 and 6 (inclusive), the count will be 
+incremented by +1, otherwise it will be incremented
+by -1.
+*/
+int increment_count(int count, int val) {
+  if (val > 2 && val < 7) {
+    return count + 1;
+  } else {
+    return count - 1;
+  }
+}
+  
 int main()
 {
   char card_name[3];
   int count = 0;
   do {
-    puts("Enter the card name: ");
-    scanf("%2s", card_name);
-    int val = 0;
-    switch(card_name[0]) {
-      case 'K':
-      case 'Q':
-      case 'J':
-        val = 10;
-        break;
-      case 'A':
-        val = 11;
-        break;
-      case 'X':
-        continue;
-      default:
-        val = atoi(card_name);
-        if (val < 1 || val > 10) {
-          puts("I don't understand that value!");
-          continue;
-        }
+    int val;
+    get_card_name(card_name);
+
+    val = get_val(card_name);
+
+    if (val == -1) {
+      puts("Couldn't understand that card name!");
+      continue;
     }
-    if (val > 2 && val < 7) {
-      count++;
-    } else {
-      count--;
+    if (val == END_FLAG) {
+      continue;
     }
+
+    count = increment_count(count, val);
+
     printf("Current count: %i\n", count);
-  } while (card_name[0] != 'X');
+  } while (card_name[0] != END_CHAR);
 
   return 0;
 }
